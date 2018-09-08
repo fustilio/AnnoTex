400405b01cb57d4940ba2d1096f8fe967678ac0c
Shannon <shanwpf@gmail.com>
2018-09-09 07:09:44 +0800

    Escape special chars

--- maketex.py
+++ maketex.py
@@ -3,20 +3,42 @@ import shutil
 import subprocess
 import glob
 import json
+import re
 
 CONST_NEWLINE = "\n"
 CONST_FILENAME = "./out/tempLatex.tex"
 
+def escapeSymbols(str):
+    conv = {
+        '&': r'\&',
+        '%': r'\%',
+        '$': r'\$',
+        '#': r'\#',
+        '_': r'\_',
+        '{': r'\{',
+        '}': r'\}',
+        '~': r'\textasciitilde{}',
+        '^': r'\^{}',
+        '\\': r'\textbackslash{}',
+        '<': r'\textless{}',
+        '>': r'\textgreater{}',
+    }
+    regex = re.compile('|'.join(re.escape(unicode(key)) for key in sorted(conv.keys(), key = lambda item: - len(item))))
+    return regex.sub(lambda match: conv[match.group()], str)
+
 def addSubTitle(filef, titleStr):
+    titleStr = escapeSymbols(titleStr)
     filef.write("\\underline{\\begin{large}\\textbf{"+ CONST_NEWLINE)
-    filef.write(titleStr + CONST_NEWLINE)
+    filef.write(titleStr.encode('utf-8') + CONST_NEWLINE)
     filef.write("}\\end{large}}" + CONST_NEWLINE + "\\newline" + CONST_NEWLINE)
 
 def addTitle(filef, titleStr):
-     filef.write(CONST_NEWLINE+ "\\section{" + titleStr + "}" +CONST_NEWLINE )
+    titleStr = escapeSymbols(titleStr)
+    filef.write(CONST_NEWLINE+ "\\section{" + titleStr.encode('utf-8') + "}" +CONST_NEWLINE )
 
 def addContent(filef, descriptionStr):
-    filef.write("$\\bullet$ " + descriptionStr + CONST_NEWLINE)
+    descriptionStr = escapeSymbols(descriptionStr)
+    filef.write("$\\bullet$ " + descriptionStr.encode('utf-8') + CONST_NEWLINE)
     filef.write("\\newline" + CONST_NEWLINE)
 
 def convertLatexToPDF():
