8beccfe76adcf0f1eab1d41e386cd49ed2ad23fe
hzxcaryn <heng.zhixuan.caryn@dhs.sg>
2018-09-09 03:45:34 +0800

    Add frontend

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'pry'
+
+include FileUtils::Verbose
 
 get '/' do
 	erb :index
+end
+
+post '/upload' do
+	#binding.pry
+    tempfile = params[:file][:tempfile] 
+    filename = params[:file][:filename] 
+    cp(tempfile.path, "out/#{filename}")
 end
